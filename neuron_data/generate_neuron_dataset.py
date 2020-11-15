61daada1129d3f4913afa2f2c592ea05cd5b6ace
xinyue96 <xixia@ucsd.edu>
2020-11-14 18:48:01 -0800

    change saving format to npz instead of npy

--- neuron_data/generate_neuron_dataset.py
+++ neuron_data/generate_neuron_dataset.py
@@ -2,6 +2,7 @@ from LIF_neurons import LIF_neurons_Sim
 import time
 import numpy as np 
 import argparse
+import scipy.sparse as sparse
 
 parser = argparse.ArgumentParser()
 
@@ -26,6 +27,8 @@ parser.add_argument('--p-exc', type=float, default=0.8,
                     help='Percentage of excitatory neurons in the simulation.')
 parser.add_argument('--epsilon', type=float, default=.1,
                     help='Probability of connection')
+parser.add_argument('--Ie_factor', type=float, default=3.0,
+                    help='External current amplitude level')
 
 
 parser.add_argument('--seed', type=int, default=0,
@@ -45,13 +48,13 @@ print(suffix)
 
 np.random.seed(args.seed)
 
-def generate_dataset(num_sims, length):
+def generate_dataset(num_sims, length,Ie_factor):
     spkmat_all = list()
     conn_all = list()
 
     for i in range(num_sims):
         t = time.time()
-        spkmat,conn = sim.simulate_network_and_spike(T=length)
+        spkmat,conn = sim.simulate_network_and_spike(T=length,Ie_factor=Ie_factor)
         if i % 100 == 0:
             print("Iter: {}, Simulation time: {}".format(i, time.time() - t))
         spkmat_all.append(spkmat)
@@ -60,31 +63,38 @@ def generate_dataset(num_sims, length):
     spkmat_all = np.stack(spkmat_all)
     conn_all = np.stack(conn_all)
 
-    return spkmat_all, conn_all
+
+    # reshaping to sparse matrix
+    sparse_spkmat_all = sparse.csr_matrix(spkmat_all.reshape(num_sims, -1))
+    sparse_conn_all   = sparse.csr_matrix(conn_all.reshape(num_sims,-1))
+
+    return sparse_spkmat_all, sparse_conn_all
+
 
 
 print("Generating {} training simulations".format(args.num_train))
 spkmat_train, conn_train = generate_dataset(args.num_train,
-                                                     args.length)
+                                                     args.length,args.Ie_factor)
                          
 
 print("Generating {} validation simulations".format(args.num_valid))
 spkmat_valid, conn_valid = generate_dataset(args.num_valid,
-                                                     args.length)
+                                                     args.length,args.Ie_factor)
 
 print("Generating {} test simulations".format(args.num_test))
 spkmat_test, conn_test = generate_dataset(args.num_test,
-                                                  args.length_test)
+                                                  args.length_test,args.Ie_factor)
 
 
-np.save('conn_train' + suffix +'_len'+str(args.length) +'.npy', conn_train)
-np.save('spkmat_train' + suffix +'_len'+str(args.length) +'.npy', spkmat_train)
 
-np.save('conn_valid' + suffix +'_len'+str(args.length) +'.npy', conn_valid)
-np.save('spkmat_valid' + suffix +'_len'+str(args.length) + '.npy', spkmat_valid)
+sparse.save_npz('conn_train' + suffix +'_len'+str(args.length) +'.npz', conn_train)
+sparse.save_npz('spkmat_train' + suffix +'_len'+str(args.length) +'.npz', spkmat_train)
 
-np.save('conn_test' + suffix +'_len'+str(args.length_test) +'.npy', conn_test)
-np.save('spkmat_test' + suffix + '_len'+str(args.length_test)+'.npy', spkmat_test)
+sparse.save_npz('conn_valid' + suffix +'_len'+str(args.length) +'.npz', conn_valid)
+sparse.save_npz('spkmat_valid' + suffix +'_len'+str(args.length) + '.npz', spkmat_valid)
+
+sparse.save_npz('conn_test' + suffix +'_len'+str(args.length_test) +'.npz', conn_test)
+sparse.save_npz('spkmat_test' + suffix + '_len'+str(args.length_test)+'.npz', spkmat_test)
 
 
 
